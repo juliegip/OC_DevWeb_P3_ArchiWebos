1b1e1df4fa534092cf7240114d3691d994c76fe5
juliegip <julie.gip@gmail.com>
2023-03-17 17:06:30 +0100

    recupération du token OK

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,36 +1,35 @@
-const loginForm = document.querySelector("#form-id").addEventListener("submit",function(e){
-    e.preventDefault();
 
-    const loginDetails = {
-            email: e.target.querySelector("[name=email]").value,
-            password: e.target.querySelector("[name=password]").value
-          };
-        console.log("LoginDetails  :",loginDetails);
+const connect = document.querySelector("#form-id").addEventListener("submit",function(e){
     
-
-
-    fetch("http://localhost:5678/api/users/login", {
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(loginDetails)
-    })
-    
-            .then(r => {
-                if (r.ok) {
-                    return r.json()
-
-                } else {
-                    throw new Error ("Identifiant/Mot de passe incorrect. Veuillez saisir à nouveau");
-                }
-            })
-            .then(data => {
-                        console.log("le body est:",data)
-                        window.location = "index.html";
-            })
-            .catch(error => {
-                console.error(error);
-                const errorElement = document.querySelector("p.msg-error");
-                errorElement.textContent ="Email/Mot de passe incorrect. Veuillez saisir à nouveau";
-            }); 
-    }); 
+        e.preventDefault();
+        const loginDetails = {
+                email: e.target.querySelector("[name=email]").value,
+                password: e.target.querySelector("[name=password]").value
+            };
+            console.log("LoginDetails  :",loginDetails);
         
+        fetch("http://localhost:5678/api/users/login", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(loginDetails)
+        })
+               .then(r => {
+                    if (r.ok) {
+                        return r.json()
+                    
+                    } else {
+                        throw new Error ("Identifiant/Mot de passe incorrect. Veuillez saisir à nouveau");
+                    }
+                })
+                .then(data => {
+                        console.log("le token est:",data.token)
+                        window.localStorage.setItem("token",data.token);
+                        window.location = "index.html";
+                })
+                .catch(error => {
+                    console.error(error);
+                    const errorElement = document.querySelector("p.msg-error");
+                    errorElement.textContent ="Email/Mot de passe incorrect. Veuillez saisir à nouveau";
+                }); 
+               
+        }        );
