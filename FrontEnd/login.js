59880e18f13eb19205565bda7c51fa71eb45006d
juliegip <julie.gip@gmail.com>
2023-03-17 11:02:44 +0100

    Ajout de la page de login

---
+++ FrontEnd/login.js
@@ -0,0 +1,36 @@
+const loginForm = document.querySelector("#form-id").addEventListener("submit",function(e){
+    e.preventDefault();
+
+    const loginDetails = {
+            email: e.target.querySelector("[name=email]").value,
+            password: e.target.querySelector("[name=password]").value
+          };
+        console.log("LoginDetails  :",loginDetails);
+    
+
+
+    fetch("http://localhost:5678/api/users/login", {
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(loginDetails)
+    })
+    
+            .then(r => {
+                if (r.ok) {
+                    return r.json()
+
+                } else {
+                    throw new Error ("Identifiant/Mot de passe incorrect. Veuillez saisir à nouveau");
+                }
+            })
+            .then(data => {
+                        console.log("le body est:",data)
+                        window.location = "index.html";
+            })
+            .catch(error => {
+                console.error(error);
+                const errorElement = document.querySelector("p.msg-error");
+                errorElement.textContent ="Email/Mot de passe incorrect. Veuillez saisir à nouveau";
+            }); 
+    }); 
+        
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -9,24 +9,24 @@ console.log(works);
 function generateWorks(works) {
     for (let i=0 ; i< works.length; i++) {
 
-const projet = works[i];
-const sectionGallery  = document.querySelector(".gallery");
-//Création d'une balise dédiée à un projet
-const elementProjet = document.createElement("figure");
-elementProjet.dataset.id = works[i].id;
+        const projet = works[i];
+        const sectionGallery  = document.querySelector(".gallery");
+        //Création d'une balise dédiée à un projet
+        const elementProjet = document.createElement("figure");
+        elementProjet.dataset.id = works[i].id;
 
-// Création des balises d'un projet
-const caption = document.createElement("figcaption");
-caption.innerText = projet.title;
-const image = document.createElement("img");
-image.src = projet.imageUrl;
+        // Création des balises d'un projet
+        const caption = document.createElement("figcaption");
+        caption.innerText = projet.title;
+        const image = document.createElement("img");
+        image.src = projet.imageUrl;
 
-// Rattachement de la balise projet à la section Gallery
-sectionGallery.appendChild(elementProjet);
+        // Rattachement de la balise projet à la section Gallery
+        sectionGallery.appendChild(elementProjet);
 
-// Rattachement des éléments d'un projet
-elementProjet.appendChild(image);
-elementProjet.appendChild(caption);
+        // Rattachement des éléments d'un projet
+        elementProjet.appendChild(image);
+        elementProjet.appendChild(caption);
 
 }
 };
@@ -35,12 +35,17 @@ generateWorks(works);
 
 
 // Gestion des filtres 
-const SelectFiltre = document.querySelectorAll(".btn-filter");
-    SelectFiltre.forEach(function(filtre){
-        filtre.addEventListener("click", function () {
-            const filtreCategoryName = filtre.textContent;
+const buttonFilters = document.querySelectorAll(".btn-filter");
+
+buttonFilters.forEach(function(selectedFilter){
+    selectedFilter.addEventListener("click", function () {
+            buttonFilters.forEach((element)=> {
+                element.classList.remove("btn-selected");
+            });
+            selectedFilter.classList.add("btn-selected");
+            const filterCategoryName = selectedFilter.textContent;
             const filteredprojects = works.filter(function(work) {
-                        return work.category.name === filtreCategoryName; 
+                        return work.category.name === filterCategoryName; 
                         
             });
             document.querySelector(".gallery").innerHTML = "";
@@ -48,9 +53,9 @@ const SelectFiltre = document.querySelectorAll(".btn-filter");
         });
     })
 
-    const SelectTous = document.querySelector(".resetfilter");
-    console.log(SelectTous.textContent);
-        SelectTous.addEventListener("click", function () {
+    const resetFilter = document.querySelector(".resetfilter");
+    console.log(resetFilter.textContent);
+    resetFilter.addEventListener("click", function () {
             document.querySelector(".gallery").innerHTML = "";
             generateWorks(works);
         });
