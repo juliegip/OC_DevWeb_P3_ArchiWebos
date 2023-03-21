e0ae3e9585bb5b262324f8ee356d3d342c5eb0d6
juliegip <julie.gip@gmail.com>
2023-03-21 22:56:33 +0100

    tout est ok pour passer à Etape 3

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,37 +1,46 @@
-
-document.querySelector("#form-id").addEventListener("submit",function(e){
+const loginAsAdmin = function(e) {
+    e.preventDefault();
     
-        e.preventDefault();
-        const loginDetails = {
-                email: e.target.querySelector("[name=email]").value,
-                password: e.target.querySelector("[name=password]").value
-            };
-            console.log("LoginDetails  :",loginDetails);
-            
+const loginDetails = {
+    email: e.target.querySelector("[name=email]").value,
+    password: e.target.querySelector("[name=password]").value
+        };
+    console.log("LoginDetails  :",loginDetails);
+
+    fetch("http://localhost:5678/api/users/login", {
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(loginDetails)
+    })
+           .then(r => {
+                if (r.ok) {
+                    return r.json()
+                
+                } else {
+                    throw new Error ("Identifiant/Mot de passe incorrect. Veuillez saisir à nouveau");
+                }
+            })
+            .then(data => {
+                    console.log("le token est:",data.token)
+                    window.sessionStorage.setItem("token",data.token);
+                    window.location.href = "index.html";
+            })
+            .catch(error => {
+                console.error(error);
+                const errorElement = document.querySelector("p.msg-error");
+                errorElement.textContent ="Email/Mot de passe incorrect. Veuillez saisir à nouveau";
+            }); 
+           
+    }       
+
+const checkbtn = document.getElementById("form-id");
+if(checkbtn) {
+    checkbtn.addEventListener("submit",loginAsAdmin);
+}else {
+        console.error("The element form-id does not exist");
+    };
+
+
+
+    
         
-        fetch("http://localhost:5678/api/users/login", {
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
-            body:JSON.stringify(loginDetails)
-        })
-               .then(r => {
-                    if (r.ok) {
-                        return r.json()
-                    
-                    } else {
-                        throw new Error ("Identifiant/Mot de passe incorrect. Veuillez saisir à nouveau");
-                    }
-                })
-                .then(data => {
-                        console.log("le token est:",data.token)
-                        window.localStorage.setItem("token",data.token);
-                        // window.location = "index.html";
-                        adminmode();
-                })
-                .catch(error => {
-                    console.error(error);
-                    const errorElement = document.querySelector("p.msg-error");
-                    errorElement.textContent ="Email/Mot de passe incorrect. Veuillez saisir à nouveau";
-                }); 
-               
-        }        );
--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,16 +1,13 @@
-let works = window.localStorage.getItem("works");
-
-const response = await fetch ('http://localhost:5678/api/works');
-works = await response.json();
-const valeurWorks = JSON.stringify(works);
-
-console.log(works);
+let token = window.sessionStorage.getItem("token");
+if (token !== undefined && token!== null){
+    adminMode()
+};
 
 function generateWorks(works) {
     for (let i=0 ; i< works.length; i++) {
 
         const projet = works[i];
-        const sectionGallery  = document.querySelector(".gallery");
+
         //Création d'une balise dédiée à un projet
         const elementProjet = document.createElement("figure");
         elementProjet.dataset.id = works[i].id;
@@ -22,40 +19,160 @@ function generateWorks(works) {
         image.src = projet.imageUrl;
 
         // Rattachement de la balise projet à la section Gallery
-        sectionGallery.appendChild(elementProjet);
+        mainGallery.appendChild(elementProjet);
 
         // Rattachement des éléments d'un projet
         elementProjet.appendChild(image);
         elementProjet.appendChild(caption);
 
-}
-};
+}};
 
-generateWorks(works);
+function adminMode(){
+
+    const adminElements = document.querySelectorAll(".admin-elements");
+        adminElements.forEach(function(e) {
+            e.classList.add("show-as-admin");
+            e.classList.remove("admin-elements")
+        });
+    const loginMenu = document.getElementById("login-menu");
+    loginMenu.remove();
+    };
+
+function logOut(){
+    const logOutElements = document.querySelectorAll(".show-as-admin");
+        logOutElements.forEach(function(e){
+            e.classList.remove("show-as-admin");
+            e.classList.add("admin-elements")
+        });
+        const loginMenu = document.createElement("li");
+        loginMenu.setAttribute("id","login-menu")
+        const anchorLogin = document.createElement("a");
+        anchorLogin.innerText = "login"
+        anchorLogin.setAttribute("href","login.html")
+        loginMenu.appendChild(anchorLogin);
+        const thirdPlace = document.querySelectorAll("#menu li")[2];
+        const listMenu = document.querySelector("#menu");
+        listMenu.insertBefore(loginMenu,thirdPlace)
+
+}
 
 
 // Gestion des filtres 
-const buttonFilters = document.querySelectorAll(".btn-filter");
+function filterGallery(event) {
+    btnFilter.forEach(btn => {
+    btn.classList.remove("btn-selected");
+  });
+    event.target.classList.add("btn-selected");
+    const filterCategoryName = event.target.textContent;
+    const worksFiltered = works.filter(function(work){
+        return work.category.name === filterCategoryName;
+    });
+    
+    console.log(filterCategoryName);
+    console.log(worksFiltered);
+    resetFilter(worksFiltered);
+}
 
-buttonFilters.forEach(function(selectedFilter){
-    selectedFilter.addEventListener("click", function () {
-            buttonFilters.forEach((element)=> {
-                element.classList.remove("btn-selected");
-            });
-            selectedFilter.classList.add("btn-selected");
-            const filterCategoryName = selectedFilter.textContent;
-            const filteredprojects = works.filter(function(work) {
-                        return work.category.name === filterCategoryName; 
-                        
-            });
-            document.querySelector(".gallery").innerHTML = "";
-            generateWorks(filteredprojects);
-        });
-    })
+const cleanGalery = function() {
+    while (mainGallery.firstChild) {
+        mainGallery.firstChild.remove()
+    };
+}
+const resetFilter = function(worksFiltered) {
+    cleanGalery();
+    generateWorks(worksFiltered);
+}
 
-    const resetFilter = document.querySelector(".resetfilter");
-    console.log(resetFilter.textContent);
-    resetFilter.addEventListener("click", function () {
-            document.querySelector(".gallery").innerHTML = "";
-            generateWorks(works);
-        });
+const btnFilter = document.querySelectorAll(".btn-filter");
+btnFilter.forEach(btn =>{btn.addEventListener("click",filterGallery)});
+document.querySelector(".resetfilter").addEventListener("click",() => resetFilter(works));
+
+
+
+// Gestion de la fenêtre modale        
+let modal = null
+
+const openModal = function (e){
+    e.preventDefault()
+    const target = document.querySelector(e.target.getAttribute("href"))
+    target.style.display = null;
+    target.removeAttribute("aria-hidden");
+    target.setAttribute("aria-modal",true);
+    modal = target
+    modal.addEventListener('click',closeModal);
+    modal.querySelector(".js-modal-close").addEventListener("click",closeModal);
+    modal.querySelector(".js-modal-stop").addEventListener("click",stopPropagation);
+    generateWorksInModal(works);
+}
+
+const closeModal = function (e){
+    if (modal == null) return
+    e.preventDefault()
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden","true");
+    modal.removeAttribute("aria-modal");
+    modal.removeEventListener("click",closeModal);
+    modal.querySelector(".js-modal-close").removeEventListener("click",closeModal);
+    modal.querySelector(".js-modal-stop").removeEventListener("click",stopPropagation);
+    modal = null
+}
+
+const stopPropagation = function (e) {
+    e.stopPropagation();
+}
+
+// Gestion de la galerie dans la modale 
+
+function generateWorksInModal(works) {
+    
+    while (modalGallery.firstChild) {
+        modalGallery.firstChild.remove()}
+
+    for (let i=0 ; i< works.length; i++) {
+
+        const projet = works[i];
+
+        //Création d'une balise dédiée à un projet
+        const elementProjet = document.createElement("figure");
+        elementProjet.dataset.id = works[i].id;
+
+        // Création des balises d'un projet
+        const iconContainer = document.createElement("div");
+        iconContainer.classList.add("icon-container");
+        iconContainer.innerHTML = `<i class="fa-solid fa-arrows-up-down-left-right"></i>
+        <i class="fa-solid fa-trash-can"></i>`        
+        const caption = document.createElement("figcaption");
+        caption.innerText = "éditer"
+        const image = document.createElement("img");
+        image.src = projet.imageUrl;
+
+        // Rattachement de la balise projet à la section Gallery
+        modalGallery.appendChild(elementProjet);
+
+        // Rattachement des éléments d'un projet
+        elementProjet.appendChild(iconContainer)
+        elementProjet.appendChild(image);
+        elementProjet.appendChild(caption);
+
+}};
+
+const response = await fetch ('http://localhost:5678/api/works');
+const works = await response.json();
+const valeurWorks = JSON.stringify(works);
+
+const mainGallery  = document.querySelector(".gallery");
+const modalGallery  = document.querySelector(".modal-gallery");
+console.log("works:", works);
+generateWorks(works);
+
+document.querySelectorAll(".js-modal").forEach(a=> {
+    a.addEventListener("click",openModal)
+});
+
+window.addEventListener("keydown",function(e){
+    if(e.key === "Escape" || e.key ==="Esc") {
+        closeModal(e)
+    }
+});
+
+document.getElementById("logout-menu").addEventListener("click",logOut);
