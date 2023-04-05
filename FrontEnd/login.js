507f0df7a7ecbdd529fbd5420827d3a91cea8b5c
juliegip <julie.gip@gmail.com>
2023-04-05 13:46:40 +0200

    modif après 1ère soutenance:formulaire login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,35 +1,35 @@
 const loginAsAdmin = function(e) {
     e.preventDefault();
     
-const loginDetails = {
-    email: e.target.querySelector("[name=email]").value,
-    password: e.target.querySelector("[name=password]").value
-        };
-    console.log("LoginDetails  :",loginDetails);
+    const loginDetails = {
+        email: e.target.querySelector("[name=email]").value,
+        password: e.target.querySelector("[name=password]").value
+            };
+        console.log("LoginDetails  :",loginDetails);
 
-    fetch("http://localhost:5678/api/users/login", {
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify(loginDetails)
-    })
-           .then(r => {
-                if (r.ok) {
-                    return r.json()
-                
-                } else {
-                    throw new Error ("Erreur dans l’identifiant ou le mot de passe");
-                }
-            })
-            .then(data => {
-                    console.log("le token est:",data.token)
-                    window.sessionStorage.setItem("token",data.token);
-                    window.location.href = "index.html";
-            })
-            .catch(error => {
-                console.error(error);
-                const errorElement = document.querySelector("p.msg-error");
-                errorElement.textContent ="Erreur dans l’identifiant ou le mot de passe";
-            }); 
+        fetch("http://localhost:5678/api/users/login", {
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(loginDetails)
+        })
+            .then(r => {
+                    if (r.ok) {
+                        return r.json()
+                    
+                    } else {
+                        throw new Error ("Erreur dans l’identifiant ou le mot de passe");
+                    }
+                })
+                .then(data => {
+                        console.log("le token est:",data.token)
+                        window.sessionStorage.setItem("token",data.token);
+                        window.location.href = "index.html";
+                })
+                .catch(error => {
+                    console.error(error);
+                    const errorElement = document.querySelector("p.msg-error");
+                    errorElement.textContent ="Erreur dans l’identifiant ou le mot de passe";
+                }); 
            
     }       
 
